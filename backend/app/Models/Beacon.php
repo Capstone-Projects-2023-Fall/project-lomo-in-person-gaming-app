553e405dfe0180545eaf838daa4878cbfd1fa79c
Alan <alan.uthuppan@temple.edu>
2023-11-13 13:20:22 -0500

    Comments relationship updated in Beacon controller class.

--- backend/app/Models/Beacon.php
+++ backend/app/Models/Beacon.php
@@ -39,6 +39,11 @@ class Beacon extends Model
         });
     }
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
