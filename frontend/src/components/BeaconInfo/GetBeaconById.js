8565f1aa8192912fd8c0b58c90a99c278907dfdb
Alan <alan.uthuppan@temple.edu>
2023-12-10 11:38:10 -0500

    Variable name changes

--- frontend/src/components/BeaconInfo/GetBeaconById.js
+++ frontend/src/components/BeaconInfo/GetBeaconById.js
@@ -3,7 +3,7 @@ import { useAuth } from "../../AuthContext.js";
 
 const GetBeaconById = (id) => {
   const { authUser } = useAuth();
-  const [beaconList, setBeaconList] = useState([]);
+  const [beaconData, setBeaconData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,7 +30,7 @@ const GetBeaconById = (id) => {
         const data = await response.json();
         console.log("single beacon response status: ", response.status);
         console.log("single beacon data retrieved: ", data.data);
-        setBeaconList(data.data[0]);
+        setBeaconData(data.data[0]);
       } catch (error) {
         console.error("Error fetching data:", error);
         // You might want to throw the error or handle it in some way
@@ -41,7 +41,7 @@ const GetBeaconById = (id) => {
     fetchData();
   }, [id, authUser]); // Add authUser as a dependency if needed
 
-  return beaconList;
+  return beaconData;
 };
 
 export default GetBeaconById;
