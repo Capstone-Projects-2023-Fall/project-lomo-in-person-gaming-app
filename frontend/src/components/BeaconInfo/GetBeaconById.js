d5bdffceaa1cd91a355c69322539e2ff53d56fdd
Alan <alan.uthuppan@temple.edu>
2023-12-10 15:22:39 -0500

    Make function give combined output with attendees combined

--- frontend/src/components/BeaconInfo/GetBeaconById.js
+++ frontend/src/components/BeaconInfo/GetBeaconById.js
@@ -30,7 +30,12 @@ const GetBeaconById = (id) => {
         const data = await response.json();
         console.log("single beacon response status: ", response.status);
         console.log("single beacon data retrieved: ", data.data);
-        setBeaconData(data.data[0]);
+        const combinedData = {
+          ...data.data[0],
+          attendees: data.data[1].attendees
+        };
+
+        setBeaconData(combinedData);
       } catch (error) {
         console.error("Error fetching data:", error);
         // You might want to throw the error or handle it in some way
