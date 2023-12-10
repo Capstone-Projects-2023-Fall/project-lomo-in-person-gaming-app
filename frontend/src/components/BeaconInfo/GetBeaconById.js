c8978c36f132b941e7929883de952916c207fa8f
Alan <alan.uthuppan@temple.edu>
2023-12-10 03:12:26 -0500

    Added id to dependency array and only retrieved first beacon object in beacon list

--- frontend/src/components/BeaconInfo/GetBeaconById.js
+++ frontend/src/components/BeaconInfo/GetBeaconById.js
@@ -30,7 +30,7 @@ const GetBeaconById = (id) => {
         const data = await response.json();
         console.log("single beacon response status: ", response.status);
         console.log("single beacon data retrieved: ", data.data);
-        setBeaconList(data.data);
+        setBeaconList(data.data[0]);
       } catch (error) {
         console.error("Error fetching data:", error);
         // You might want to throw the error or handle it in some way
@@ -39,7 +39,7 @@ const GetBeaconById = (id) => {
     };
 
     fetchData();
-  }, [authUser]); // Add authUser as a dependency if needed
+  }, [id, authUser]); // Add authUser as a dependency if needed
 
   return beaconList;
 };
