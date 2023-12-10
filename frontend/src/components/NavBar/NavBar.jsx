10b560565ecc99ae9d37b14a0aa747c742b2d831
JonathanAltenburg <jaltenburg219@gmail.com>
2023-12-10 13:04:21 -0500

    Page on click on avatar display

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect, onClick } from 'react';
 import { Link } from 'react-router-dom';
 import GetUserById from "../BeaconInfo/GetUserById.js";
 import { useAuth } from '../../AuthContext.js';
 import './NavBar.css';
+import ProfileUpdate from '../Profile/ProfileUpdate.js';
 
 const NavBar = () => {
     const { logout } = useAuth();
     const { authUser, userId } = useAuth();
     const userInfo = GetUserById(userId);
     const picClass = "h-7 w-7 rounded-full ";
+    const [showProfEdit, setShowProfEdit] = useState(false);
 
     const handleLogout = () => {
         logout();
+    }
+
+
+    function changeProfile(){
+        setShowProfEdit(true);
     }
 
     return (
@@ -24,8 +31,14 @@ const NavBar = () => {
                     <ul className='flex space-x-4 text-teal-50'>
                         
                         <h3 className="text-l text-left">Welcome, {userInfo.username}!</h3>
-                        
+                        <button onClick={() => changeProfile()}>
+                        {showProfEdit &&
+                            <ProfileUpdate setShowProfEdit={setShowProfEdit} />
+                        }
                         <img src={userInfo.avatar || "icons/defaultPFP.jpg"} alt="User Pic" className={picClass} />
+
+                        </button>
+                        
                         
                     
                         
